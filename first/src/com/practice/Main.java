0e7c0f333d588f14a42866c931b80b0b8e513923
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-11-23 19:20:45 -0800

    push

---
+++ first/src/com/practice/Main.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        System.out.println(day);
+    }
+}
