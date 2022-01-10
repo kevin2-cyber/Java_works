5588a3bea1eb0ad22ad6e0ba1c6077434f0557da
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-01-10 23:49:59 +0000

    created a method function that returns a string literal

--- first/src/com/practice/Main.java
+++ first/src/com/practice/Main.java
@@ -1,11 +1,19 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
         int day = scanner.nextInt();
-        System.out.println(day);
+        String name = scanner.nextLine();
+        String funcName = function(name);
+        System.out.println(funcName + " is " + day + " years old");
+    }
+
+    public static String function(String name) {
+        return name;
     }
 }
