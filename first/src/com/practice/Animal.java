76f278d0323d94edf1209311ceb767fc04a4a724
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-01-11 00:17:45 +0000

    working with classes

---
+++ first/src/com/practice/Animal.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class Animal {
+
+    private String Amoeba() {
+        return "Amoeba Proteus";
+    }
+
+    public String Protista() {
+        return Amoeba();
+    }
+}
--- first/src/com/practice/Main.java
+++ first/src/com/practice/Main.java
@@ -7,13 +7,23 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
-        int day = scanner.nextInt();
-        String name = scanner.nextLine();
-        String funcName = function(name);
-        System.out.println(funcName + " is " + day + " years old");
+        // int day = scanner.nextInt();
+        // String name = scanner.nextLine();
+        // String funcName = function(name);
+        // System.out.println(funcName + " is " + day + " years old");
+        // int car = comeName(20);
+        // System.out.println(car);
+        Animal animal = new Animal();
+        String protist = animal.Protista();
+        System.out.println(protist);
     }
+
 
     public static String function(String name) {
         return name;
+    }
+
+    private static int comeName(int come) {
+        return come;
     }
 }
