9b1b8b3da1dd4cac4a157c5e7e49de4ec92be9ab
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-01-25 15:00:24 +0000

    created a new class

---
+++ trial/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Labclass labclass = new Labclass();
+        System.out.println(labclass.numberofStudents());
+    }
+}
+
+class Labclass {
+    public int numberofStudents() {
+        int dog = 5;
+        return 0;
+    }
+}
