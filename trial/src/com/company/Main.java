1ae7d09ad255bd677184e243acaa3a166bea51b3
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-23 15:50:26 +0000

    used var as a keyword

--- trial/src/com/company/Main.java
+++ trial/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        var name = "Hello";
+        System.out.println(name);
         System.out.println("Hello World");
     }
 }
