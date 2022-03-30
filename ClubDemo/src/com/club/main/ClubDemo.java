9b1babc1e2be9e6195ce379e183960f7e7488283
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-30 23:27:25 +0000

    almost done with it

--- ClubDemo/src/com/club/main/ClubDemo.java
+++ ClubDemo/src/com/club/main/ClubDemo.java
@@ -1,5 +1,24 @@
 package com.club.main;
 
 public class ClubDemo {
-
+    private Club club;
+    public ClubDemo() {
+        club = new Club();
+    }
+    public void demo() {
+        club = new Club();
+        club.join(new Membership("Gordy Broon", 1, 1972));
+        club.join(new Membership("Eck Salmon", 9, 1965));
+        club.join(new Membership("Davie Caramel", 5, 1960));
+        System.out.println("Using for-each ....");
+        club.listMembers1();
+        System.out.println("Using while loop....");
+        club.listMembers2();
+        System.out.println("Using iteraror...");
+        club.listMembers3();
+        System.out.println("Now doing some searching...");
+        club.searchMember("mon");
+        System.out.println("And some more searching...");
+        club.searchMember("moon");
+    }
 }
