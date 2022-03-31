bb870b779fa89ebde1795710bd38dc9bdf164be4
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-31 19:51:39 +0000

    help

--- WebLogOrganizer/src/com/weblog/analyzer/LoglineTokenizer.java
+++ WebLogOrganizer/src/com/weblog/analyzer/LoglineTokenizer.java
@@ -1,5 +1,20 @@
 package com.weblog.analyzer;
 
-public class LoglineTokenizer {
+import java.util.Scanner;
 
+public class LoglineTokenizer {
+    public LoglineTokenizer() {}
+    public void tokenize(String logline, int[] dataLine) {
+        try {
+            // Scan the logline for integers.
+            Scanner tokenizer = new Scanner(logline);
+            for(int i = 0; i < dataLine.length; i++) {
+                dataLine[i] = tokenizer.nextInt();
+            }
+        }
+        catch(java.util.NoSuchElementException e) {
+            System.out.println("Insuffient data items on log line: " + logline);
+            throw e;
+        }
+    }
 }
