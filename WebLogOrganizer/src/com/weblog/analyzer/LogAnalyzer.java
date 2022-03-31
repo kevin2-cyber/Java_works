7675b034f6f6b4476682f4d58cd65694b6e07f3b
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-31 23:12:48 +0000

    starter data

--- WebLogOrganizer/src/com/weblog/analyzer/LogAnalyzer.java
+++ WebLogOrganizer/src/com/weblog/analyzer/LogAnalyzer.java
@@ -1,5 +1,37 @@
 package com.weblog.analyzer;
 
 public class LogAnalyzer {
+    // Where to calculate the hourly access counts.
+    private int[] hourCounts;
+    // Use a LogfileReader to access the data.
+    private LogfileReader reader;
+
+    public LogAnalyzer() {
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader();
+    }
+
+    public void analyzeHourlyData() {
+        while(reader.hasNext()) {
+            LogEntry entry = reader.next();
+            int hour = entry.getHour();
+            hourCounts[hour]++;
+        }
+    }
+
+    public void printHourlyCounts() {
+        System.out.println("Hr: Count");
+        for(int hour = 0; hour < hourCounts.length; hour++) {
+            System.out.println(hour + ": " + hourCounts[hour]);
+        }
+    }
+
+    public void printData() {
+        reader.printData();
+    }
+
 
 }
