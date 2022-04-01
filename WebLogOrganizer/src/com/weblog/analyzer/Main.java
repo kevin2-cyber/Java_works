0585e29067c609e8f5690438ea81863def46ad33
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-01 04:58:43 +0000

    created LogAnalyzer object

--- WebLogOrganizer/src/com/weblog/analyzer/Main.java
+++ WebLogOrganizer/src/com/weblog/analyzer/Main.java
@@ -2,6 +2,7 @@ package com.weblog.analyzer;
 
 public class Main {
     public static void main(String[] args) {
-
+    LogAnalyzer logAnalyzer = new LogAnalyzer();
+    logAnalyzer.printHourlyCounts();
     }
 }
