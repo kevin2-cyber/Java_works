f47bcc5ec8238961b054d001a48e80b9866db6a8
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-04-02 23:52:26 +0000

    Update Main.java

--- WebLogOrganizer/src/com/weblog/analyzer/Main.java
+++ WebLogOrganizer/src/com/weblog/analyzer/Main.java
@@ -3,10 +3,5 @@ package com.weblog.analyzer;
 public class Main {
     public static void main(String[] args) {
     LogAnalyzer logAnalyzer = new LogAnalyzer();
-    logAnalyzer.numberOfAccesses();
-    logAnalyzer.printData();
-    logAnalyzer.printHourlyCounts();
-    logAnalyzer.quietHour();
-    logAnalyzer.analyzeHourlyData();
     }
 }
