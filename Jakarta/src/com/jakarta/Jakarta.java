14b98dd66822102124d73dd3f391ab7b4d5170bf
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-03 13:09:04 +0000

    printed first variable integer

--- Jakarta/src/com/jakarta/Jakarta.java
+++ Jakarta/src/com/jakarta/Jakarta.java
@@ -1,7 +1,12 @@
 package com.jakarta;
 
+import java.util.Arrays;
+
 public class Jakarta {
     public static void main(String[] args) {
         System.out.println("Hello, Kelvin!");
+
+        int myFirstNumber = 5;
+        System.out.println("myFirstNumber = " + myFirstNumber);
     }
 }
