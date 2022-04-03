dd81c2fac781d3d8c911fc32f9f4305ac5099ee9
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-03 12:56:50 +0000

    Hello world

--- Jakarta/src/com/jakarta/Jakarta.java
+++ Jakarta/src/com/jakarta/Jakarta.java
@@ -2,6 +2,6 @@ package com.jakarta;
 
 public class Jakarta {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Kelvin!");
     }
 }
