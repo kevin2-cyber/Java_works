724e0d4a56e2b0354df7a1b03d98cc60b014e8b9
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-03 22:28:58 +0000

    addition

--- Jakarta/src/com/jakarta/Jakarta.java
+++ Jakarta/src/com/jakarta/Jakarta.java
@@ -7,6 +7,9 @@ public class Jakarta {
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
-        System.out.println("The number is " + myFirstNumber);
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println("The number is " + myTotal);
     }
 }
