f9bb7881900ab2ac927c4908097fde0d34823065
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 18:22:24 +0000

    printing max and min values

--- Jakarta/src/main/jakarta/Main.java
+++ Jakarta/src/main/jakarta/Main.java
@@ -2,6 +2,10 @@ package main.jakarta;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("myMaxIntValue = " + myMaxIntValue);
+        System.out.println("myMinIntValue = " + myMinIntValue);
     }
 }
