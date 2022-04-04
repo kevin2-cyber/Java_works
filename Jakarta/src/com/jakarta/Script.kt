144cbaaee42f88f036a36800a55f815e058f448c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 23:37:46 +0000

    added setter methods in kotlin files

--- Jakarta/src/com/jakarta/Script.kt
+++ Jakarta/src/com/jakarta/Script.kt
@@ -10,4 +10,12 @@ class Script(data: Int, money: Int) : Dao(data, money) {
     override fun getMoney(): Int {
         return super.getMoney()
     }
+
+    override fun setData(data: Int) {
+        super.setData(data)
+    }
+
+    override fun setMoney(money: Int) {
+        super.setMoney(money)
+    }
 }
