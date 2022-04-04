e20cbaee07334f9e59ed77e537f3f6a0ef3f68d3
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 22:30:50 +0000

    override method in Dao class

--- Jakarta/src/com/jakarta/Script.kt
+++ Jakarta/src/com/jakarta/Script.kt
@@ -2,7 +2,7 @@ package com.jakarta
 
 import main.jakarta.Main
 
-class Script : Main() {
+open class Script : Main() {
     override fun printData(data: String?) {
         super.printData("Hello")
     }
---
+++ Jakarta/src/main/jakarta/Dao.java
@@ -0,0 +1,10 @@
+package main.jakarta;
+
+import com.jakarta.Script;
+
+public class Dao extends Script {
+    @Override
+    public void printData(String data) {
+        super.printData(data);
+    }
+}
