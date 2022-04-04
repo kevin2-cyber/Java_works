309c16719d973bae12cc0140f2e26e9801b681d3
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 23:18:24 +0000

    added getter methods

--- Jakarta/src/main/jakarta/Dao.java
+++ Jakarta/src/main/jakarta/Dao.java
@@ -4,5 +4,19 @@ package main.jakarta;
 
 
 public class Dao {
+    private int data;
+    private int money;
 
+    public Dao(int data, int money){
+        this.data = data;
+        this.money = money;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
