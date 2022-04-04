d66e4a30a3d51560ab8ab43e5468f934bff45c2a
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 19:12:48 +0000

    comments

--- Jakarta/src/main/jakarta/Main.java
+++ Jakarta/src/main/jakarta/Main.java
@@ -2,13 +2,14 @@ package main.jakarta;
 
 public class Main {
     public static void main(String[] args) {
-        byte myByte = 19;
-        short myShort = 20;
-        int myInt = 200;
-        long longTotal = 50000 + (10 * (myByte + myShort + myInt));
-        System.out.println("longTotal = " + longTotal);
+//        byte myByte = 19;
+//        short myShort = 20;
+//        int myInt = 200;
+//        long longTotal = 50000 + (10 * (myByte + myShort + myInt));
+//        System.out.println("longTotal = " + longTotal);
+//
+//        short shortTotal = (short) (10000 + 10 * (myByte + myShort + myInt));
+//        System.out.println("shortTotal = " + shortTotal);
 
-        short shortTotal = (short) (10000 + 10 * (myByte + myShort + myInt));
-        System.out.println("shortTotal = " + shortTotal);
     }
 }
