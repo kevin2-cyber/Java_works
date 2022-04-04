795e18ae8aceaa597ea15b690b8bc78df99eec7b
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-04 23:26:31 +0000

    override getters

--- Jakarta/src/com/jakarta/Script.kt
+++ Jakarta/src/com/jakarta/Script.kt
@@ -3,5 +3,11 @@ package com.jakarta
 import main.jakarta.Dao
 
 class Script(data: Int, money: Int) : Dao(data, money) {
+    override fun getData(): Int {
+        return super.getData()
+    }
 
+    override fun getMoney(): Int {
+        return super.getMoney()
+    }
 }
