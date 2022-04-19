322d4fec04792aceae1ded3af5ffe8c25e745ff7
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-19 23:59:44 +0000

    k

--- Jakarta/src/main/micro/DataTypes.java
+++ Jakarta/src/main/micro/DataTypes.java
@@ -4,6 +4,6 @@ public class DataTypes {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         String name = "Hello";
-        System.out.println("length of the name variable is " + name.length());
+        System.out.println("length of the name is " + name.length());
     }
 }
