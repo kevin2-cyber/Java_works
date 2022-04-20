a224ac10a6bb21b327dfd115d0b13a84ae0c9fef
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-20 00:50:13 +0000

    string

--- Jakarta/src/main/micro/DataTypes.java
+++ Jakarta/src/main/micro/DataTypes.java
@@ -4,5 +4,9 @@ public class DataTypes {
     public static void main(String[] args) {
       String myString = "This is a string";
         System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
     }
 }
