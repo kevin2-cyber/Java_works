3bdc079cd0d73a6252a797bbc7d960f1a11f6a00
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-20 01:01:16 +0000

    hey

--- Jakarta/src/main/micro/DataTypes.java
+++ Jakarta/src/main/micro/DataTypes.java
@@ -8,5 +8,8 @@ public class DataTypes {
         System.out.println("myString is equal to " + myString);
         myString = myString + " \u00A9 2019";
         System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.55";
+        System.out.println("numberString = " + numberString);
     }
 }
