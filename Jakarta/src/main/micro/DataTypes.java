ff87c99498f44c259b9e7d1528c270b393d22e26
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-20 00:17:19 +0000

    strings

--- Jakarta/src/main/micro/DataTypes.java
+++ Jakarta/src/main/micro/DataTypes.java
@@ -2,8 +2,7 @@ package main.micro;
 
 public class DataTypes {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        String name = "Hello";
-        System.out.println("length of the name is " + name.length());
+      String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
     }
 }
