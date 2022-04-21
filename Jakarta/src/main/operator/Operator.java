eaeb8e648cfeeafb63ff79b8915b3f240b8f0da8
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-21 16:32:11 +0000

    operators

--- Jakarta/src/main/operator/Operator.java
+++ Jakarta/src/main/operator/Operator.java
@@ -3,5 +3,6 @@ package main.operator;
 public class Operator {
     public static void main(String[] args) {
         int result = 1 + 1;
+        System.out.println("result = " + result);
     }
 }
