c2302efbf272dd2a59795d500b5d2ff88e5744aa
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-20 23:13:05 +0000

    new

---
+++ Jakarta/src/main/exam/Exam.java
@@ -0,0 +1,11 @@
+package main.exam;
+
+public class Exam {
+    public static void main(String[] args) {
+        int[] array = {120, 200, 016};
+
+        for (int j : array) {
+            System.out.println("array = " + j);
+        }
+    }
+}
