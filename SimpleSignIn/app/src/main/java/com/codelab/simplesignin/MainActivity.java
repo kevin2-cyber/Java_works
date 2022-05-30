f1a2436d7908657ed2994beb0e68ac629b8410df
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-30 07:18:43 +0000

    done

--- SimpleSignIn/app/src/main/java/com/codelab/simplesignin/MainActivity.java
+++ SimpleSignIn/app/src/main/java/com/codelab/simplesignin/MainActivity.java
@@ -3,6 +3,12 @@ package com.codelab.simplesignin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(
+                v -> {
+                    if (username.getText().toString().equals("admin") && password.getText().toString().equals("password")) {
+                        Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
