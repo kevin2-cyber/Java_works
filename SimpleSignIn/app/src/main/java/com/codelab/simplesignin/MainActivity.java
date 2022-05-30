2accbe34eefd9b7b6346a6dbdd66e1d4cc4ded22
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-30 13:02:10 +0000

    added viewBinding

--- SimpleSignIn/app/src/main/java/com/codelab/simplesignin/MainActivity.java
+++ SimpleSignIn/app/src/main/java/com/codelab/simplesignin/MainActivity.java
@@ -4,26 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import com.codelab.simplesignin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
 
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
 
-        MaterialButton loginBtn = findViewById(R.id.loginbtn);
 
-        loginBtn.setOnClickListener(
+        binding.loginbtn.setOnClickListener(
                 view -> {
-                    if (username.getText().toString().equals("admin") && password.getText().toString().equals("password")) {
+                    if (binding.username.getText().toString().equals("admin") && binding.password.getText().toString().equals("password")) {
                         startActivity(new Intent(this, WelcomeActivity.class));
                         Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
                     } else {
--- SimpleSignIn/app/src/main/java/com/codelab/simplesignin/WelcomeActivity.java
+++ SimpleSignIn/app/src/main/java/com/codelab/simplesignin/WelcomeActivity.java
@@ -6,20 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.codelab.simplesignin.databinding.ActivityWelcomeBinding;
 import com.google.android.material.button.MaterialButton;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private ActivityWelcomeBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mBinding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(mBinding.getRoot());
 
-
-        MaterialButton button = findViewById(R.id.logout_btn);
-        TextView textView = findViewById(R.id.input_text);
-
-        button.setOnClickListener(view -> {
+        mBinding.logoutBtn.setOnClickListener(view -> {
             startActivity(new Intent(this, MainActivity.class));
         });
     }
