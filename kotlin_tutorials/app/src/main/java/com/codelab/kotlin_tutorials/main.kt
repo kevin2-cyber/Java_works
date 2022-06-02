c217631900b2510d3cf54b5dc9c2f4fc24ba36cf
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-02 06:13:55 +0000

    integer data types

--- kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
+++ kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
@@ -6,5 +6,16 @@ fun main() {
     name = "Eduful"
     // immutable variables
     val myName = "Kelvin"
+
+    var myAge = 31
+
+    // Byte (8 bit)
+    val byte: Byte = 13
+    // Short (16 bit)
+    val short: Short = 125
+    // Int (32 bit)
+    val int: Int = 123123123
+    // Long (64 bit)
+    val long: Long = 39_812_309_487_120_300
     print("Hello $myName $name")
 }
