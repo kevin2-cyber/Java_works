052a7a730580b6fa03f9a9e6d4cd5de43a5cb995
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-02 19:12:25 +0000

    a practical application of while loop

--- kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
+++ kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
@@ -1,82 +1,82 @@
 package com.codelab.kotlin_tutorials
 
 fun main() {
-    // mutable variables
-    var name = "Baidoo"
-    name = "Eduful"
-    // immutable variables
-    val myName = "Kelvin"
-
-    // type inference finds out the type from context
-    var myAge = 31
-
-    // Integer types
-    // Byte (8 bit)
-    val byte: Byte = 13
-    // Short (16 bit)
-    val short: Short = 125
-    // Int (32 bit)
-    val int: Int = 123123123
-    // Long (64 bit)
-    val long: Long = 39_812_309_487_120_300
-
-    // Floating Point number Types
-    // Float (32 bit)
-    val float: Float = 13.37F
-    // Double (64 bit)
-    val double: Double = 3.14159265358979323846
-
-    // Booleans is used to represent logical values
-    // It can have two possible values true and false
-    var isSunny: Boolean = true
-    isSunny = false
-
-    // Characters
-    val letterChar = 'A'
-    val digitChar = '1'
-
-    // Strings
-    val myStr = "Hello World"
-    val firstCharInStr = myStr[0]
-    val lastCharInStr = myStr[myStr.length - 1]
-
-   // print("first character is $firstCharInStr and the length of myStr is ${myStr.length}")
-
-    // Arithmetic operators (+, -, *, /, %)
-    var result = 5 + 3
-    val a = 5.0
-    val b = 3
-    var resultDouble: Double
-    resultDouble = a / b
-    // print(resultDouble)
-
-    // Comparison operators (==, !=, <, >, <=, >=)
-    val isEqual = 5 == 3
-    // println("isEqual is $isEqual")
-
-    val isNotEqual = 5 != 5
-    // println("isNotEqual is $isNotEqual")
-
-//    println("is5greater3 ${5 > 3}")
-//    println("is-5greater3 ${-5 > 3}")
-//    println("is5LowerEqual3 ${5 <= 3}")
-//    println("is5GreaterEqual3 ${5 >= 3}")
-
-    // Assignment operators (+=, -=, *=, /=, %=)
-    var myNum = 5
-    myNum += 3
-    myNum *= 4
-    // println("myNum is $myNum")
-
-    // Increment & Decrement operators (++, --)
-    myNum++
-    // println("myNum is $myNum")
-    // println("myNum is ${myNum++}")
-    // println("myNum is ${++myNum}")
-    // println("myNum is ${--myNum}")
-
-    val heightPerson1 = 170
-    val heightPerson2 = 189
+//    // mutable variables
+//    var name = "Baidoo"
+//    name = "Eduful"
+//    // immutable variables
+//    val myName = "Kelvin"
+//
+//    // type inference finds out the type from context
+//    var myAge = 31
+//
+//    // Integer types
+//    // Byte (8 bit)
+//    val byte: Byte = 13
+//    // Short (16 bit)
+//    val short: Short = 125
+//    // Int (32 bit)
+//    val int: Int = 123123123
+//    // Long (64 bit)
+//    val long: Long = 39_812_309_487_120_300
+//
+//    // Floating Point number Types
+//    // Float (32 bit)
+//    val float: Float = 13.37F
+//    // Double (64 bit)
+//    val double: Double = 3.14159265358979323846
+//
+//    // Booleans is used to represent logical values
+//    // It can have two possible values true and false
+//    var isSunny: Boolean = true
+//    isSunny = false
+//
+//    // Characters
+//    val letterChar = 'A'
+//    val digitChar = '1'
+//
+//    // Strings
+//    val myStr = "Hello World"
+//    val firstCharInStr = myStr[0]
+//    val lastCharInStr = myStr[myStr.length - 1]
+//
+//   // print("first character is $firstCharInStr and the length of myStr is ${myStr.length}")
+//
+//    // Arithmetic operators (+, -, *, /, %)
+//    var result = 5 + 3
+//    val a = 5.0
+//    val b = 3
+//    var resultDouble: Double
+//    resultDouble = a / b
+//    // print(resultDouble)
+//
+//    // Comparison operators (==, !=, <, >, <=, >=)
+//    val isEqual = 5 == 3
+//    // println("isEqual is $isEqual")
+//
+//    val isNotEqual = 5 != 5
+//    // println("isNotEqual is $isNotEqual")
+//
+////    println("is5greater3 ${5 > 3}")
+////    println("is-5greater3 ${-5 > 3}")
+////    println("is5LowerEqual3 ${5 <= 3}")
+////    println("is5GreaterEqual3 ${5 >= 3}")
+//
+//    // Assignment operators (+=, -=, *=, /=, %=)
+//    var myNum = 5
+//    myNum += 3
+//    myNum *= 4
+//    // println("myNum is $myNum")
+//
+//    // Increment & Decrement operators (++, --)
+//    myNum++
+//    // println("myNum is $myNum")
+//    // println("myNum is ${myNum++}")
+//    // println("myNum is ${++myNum}")
+//    // println("myNum is ${--myNum}")
+//
+//    val heightPerson1 = 170
+//    val heightPerson2 = 189
 
 //    if (heightPerson1 > heightPerson2) {
 //        println("use raw force")
@@ -166,5 +166,15 @@ fun main() {
         x++
     }while (x <= 10)
     println("\ndo while loop is done")
+
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while (feltTemp == "cold"){
+        roomTemp++
+        if (roomTemp >= 20) {
+            feltTemp = "comfy"
+            println("it's comfy now")
+        }
+    }
 
 }
