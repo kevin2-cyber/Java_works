9b59a9bfd6828f836b374315e8e85e53e3bf26c2
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-02 05:50:58 +0000

    difference between var and val

--- kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
+++ kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
@@ -1,17 +1,8 @@
 package com.codelab.kotlin_tutorials
 
 fun main() {
-    val man = Man("Kwaku",22)
-    val woman = Woman("Adwoa", 21)
-    val male = Male("ForeLimbs", "Hind limbs")
-    val female = Female("Hand", "Leg")
-
-    println(male.lengthOfHands())
-    println(male.lengthOfLegs())
-    println(male.lengthOfBoth())
-    println(female.lengthOfHands())
-    println(female.lengthOfLegs())
-    println(female.lengthOfBoth())
-    println(woman.addProp())
-    println(man.addProp())
+    var name = "Baidoo"
+    name = "Eduful"
+    val myName = "Kelvin"
+    print("Hello $myName $name")
 }
