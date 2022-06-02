dccd5b54db1f04627bbc8304519849b329cfc4e1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-02 06:03:46 +0000

    mutable and immutable variables

--- kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
+++ kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
@@ -1,8 +1,10 @@
 package com.codelab.kotlin_tutorials
 
 fun main() {
+    // mutable variables
     var name = "Baidoo"
     name = "Eduful"
+    // immutable variables
     val myName = "Kelvin"
     print("Hello $myName $name")
 }
