911f3222d0bb4852c060d64ab721e06140166667
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-02 06:32:47 +0000

    Floating point numbers

--- kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
+++ kotlin_tutorials/app/src/main/java/com/codelab/kotlin_tutorials/main.kt
@@ -9,6 +9,7 @@ fun main() {
 
     var myAge = 31
 
+    // Integer types
     // Byte (8 bit)
     val byte: Byte = 13
     // Short (16 bit)
@@ -17,5 +18,11 @@ fun main() {
     val int: Int = 123123123
     // Long (64 bit)
     val long: Long = 39_812_309_487_120_300
+
+    // Floating Point number Types
+    // Float (32 bit)
+    val float: Float = 13.37
+    // Double (64 bit)
+    val double: Double = 3.14159265358979323846
     print("Hello $myName $name")
 }
