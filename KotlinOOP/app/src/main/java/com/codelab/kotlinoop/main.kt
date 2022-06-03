72a9e4d79c0e2e1185db197725703b44902e704d
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-03 19:13:24 +0000

    exercise on classes and initializers

--- KotlinOOP/app/src/main/java/com/codelab/kotlinoop/main.kt
+++ KotlinOOP/app/src/main/java/com/codelab/kotlinoop/main.kt
@@ -4,6 +4,10 @@ fun main() {
     var kevin = Person("Kelvin", "Eduful")
     var john = Person()
     var johnPeterson = Person(lastName = "Peterson")
+
+    var samsung = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
+    var iPhone = MobilePhone("iOS", "Apple", "13 Pro Max")
+    var nokia = MobilePhone("Android", "Nokia", "Nokia Lumia")
 }
 
 class Person (firstName: String = "John", lastName: String = "Doe") {
@@ -11,6 +15,15 @@ class Person (firstName: String = "John", lastName: String = "Doe") {
     // Initializer Block
     init {
         println("Initialized a new Person object with " +
-        "firstName = $firstName and lastName = $lastName")
+        "\nfirstName = $firstName \nand lastName = $lastName")
+    }
+}
+
+class MobilePhone (osName: String, brand: String, model: String) {
+
+    init {
+        println("OS name is $osName" +
+                "\nbrand is $brand" +
+                "\nmodel is $model")
     }
 }
