db97eac06bf71f041927e51467c2093eddb1dacb
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-06 19:42:55 +0000

    arrays

--- Collections/app/src/main/java/com/codelab/collections/Basics.kt
+++ Collections/app/src/main/java/com/codelab/collections/Basics.kt
@@ -6,7 +6,7 @@ fun main() {
     // val numbers = intArrayOf(1,2,3,4,5,6)
 
     val numbers = arrayOf(1,2,3,4,5,6)
-    val numbersD: DoubleArray = doubleArrayOf()
+    val numbersD: DoubleArray = doubleArrayOf(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
     // print(numbers.contentToString())
     // for (element in numbers) {
       //  print(" ${element + 2}")
@@ -19,4 +19,28 @@ fun main() {
 
     print("\nfinal values ${numbers.contentToString()}")
 
+    numbersD[0] = 6.0
+    numbersD[1] = 5.0
+    numbersD[4] = 2.0
+    numbersD[5] = 1.0
+
+
+    // print(days.contentToString())
+
+    val fruits = arrayOf(
+        Fruit("Apple", 2.5),
+        Fruit("Grape",3.5))
+    // print(fruits.contentToString())
+
+    for (fruit in fruits){
+        print(" ${fruit.name}")
+    }
+    for (index in fruits.indices) {
+        print("${fruits[index].name} is in the index $index")
+    }
+
+    val mix = arrayOf("Sun", "Mon", "Tues",1,2,3,Fruit("Apple",2.5))
+    print(mix.contentToString())
 }
+
+data class Fruit(val name: String, val price: Double)
