8afb97bbbb96912d74416a9f9a172e962ef0e1e1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-09 17:08:57 +0000

    initial commit with tests written for a separate functionality

---
+++ AgeInMinutes/app/src/main/java/com/codelab/ageinminutes/KotlinUser.kt
@@ -0,0 +1,3 @@
+package com.codelab.ageinminutes
+
+data class KotlinUser(var first: String, var last: String)
---
+++ AgeInMinutes/app/src/test/java/com/codelab/ageinminutes/UserUnitTest.kt
@@ -0,0 +1,45 @@
+package com.codelab.ageinminutes
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class UserUnitTest {
+
+    private val first = "Alice"
+    private val last = "Simmons"
+    private val user = JavaUser(first, last)
+    private val userSame = JavaUser(first, last)
+    private val userDifferent = JavaUser("Alex", "Smith")
+
+    @Test
+    fun readProperties() {
+        assertEquals(first, user.first)
+        assertEquals(last, user.last)
+    }
+
+    @Test
+    fun writeProperties() {
+        user.first = "Alex"
+        user.last = "Smith"
+
+        assertNotEquals(first,user.first)
+        assertNotEquals(last,user.last)
+    }
+
+    @Test
+    fun checkHashCode() {
+        assertNotEquals(user.hashCode(),userDifferent.hashCode())
+    }
+
+    @Test
+    fun checkEquals() {
+        assert(user == userSame)
+    }
+
+    @Test
+    fun checkNotEquals() {
+        assert(user != userDifferent)
+    }
+
+}
