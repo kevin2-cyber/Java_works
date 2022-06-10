e92814969f92d9213ea598f65614870abf73e38f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-10 14:04:40 +0000

    enabled the supportActionBar

--- AgeInMinutes/app/src/main/java/com/codelab/ageinminutes/MainActivity.kt
+++ AgeInMinutes/app/src/main/java/com/codelab/ageinminutes/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        // supportActionBar?.hide()
+        supportActionBar?.hide()
     }
 
     private fun clickDatePicker(view: View) {
