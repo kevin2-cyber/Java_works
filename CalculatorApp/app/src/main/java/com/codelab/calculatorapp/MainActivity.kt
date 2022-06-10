e4ad1de98ee5d75deb3b660186153799aaa66fd1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-10 19:02:39 +0000

    Building the calculator ui using linear layouts

--- CalculatorApp/app/src/main/java/com/codelab/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/codelab/calculatorapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.codelab.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
     }
 }
