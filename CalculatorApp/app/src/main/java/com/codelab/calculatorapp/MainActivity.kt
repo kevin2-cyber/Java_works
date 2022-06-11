def6ab1b41a3bbbfa5e650abfa12c6df02aabdc7
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-11 10:36:17 +0000

    Added the onOperator functionality using contains and startWith on Strings

--- CalculatorApp/app/src/main/java/com/codelab/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/codelab/calculatorapp/MainActivity.kt
@@ -35,4 +35,25 @@ class MainActivity : AppCompatActivity() {
             lastDot = true
         }
     }
+
+    fun onOperator(view: View) {
+        binding.tvInput.text.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                binding.tvInput.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
+    }
 }
