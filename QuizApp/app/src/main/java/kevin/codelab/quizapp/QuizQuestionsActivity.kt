71cbb0a01796b0c88ad241fe03469bcb75ac2b9a
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-15 11:19:32 +0000

    created the questions model and prepared the questions

--- QuizApp/app/src/main/java/kevin/codelab/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/kevin/codelab/quizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,13 @@ package kevin.codelab.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
-    }
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Question size is ", "${questionsList.size}")    }
 }
