2c2cb321787fb15d7b67550e34d3e1b1d1191c3f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-15 18:03:46 +0000

    connecting the ui to the model

--- QuizApp/app/src/main/java/kevin/codelab/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/kevin/codelab/quizapp/QuizQuestionsActivity.kt
@@ -3,13 +3,32 @@ package kevin.codelab.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kevin.codelab.quizapp.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        setContentView(binding.root)
 
         val questionsList = Constants.getQuestions()
         Log.i("Question size is ", "${questionsList.size}")
+
+        for (i in questionsList) {
+            Log.e("Questions", i.questions)
+        }
+
+        var currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+        binding.progressBar.progress = currentPosition
+        binding.ivImage.setImageResource(question.image)
+        binding.tvProgress.text = "$currentPosition/${binding.progressBar.max}"
+        binding.tvQuestion.text = question.questions
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
     }
 }
