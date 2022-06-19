da9f8e592d10123df74fae7b2e7495afbc4289c7
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-19 15:43:46 +0000

    added viewBinding

--- QuizApp/app/src/main/java/kevin/codelab/quizapp/ResultActivity.kt
+++ QuizApp/app/src/main/java/kevin/codelab/quizapp/ResultActivity.kt
@@ -2,10 +2,15 @@ package kevin.codelab.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kevin.codelab.quizapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityResultBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(binding.root)
     }
 }
