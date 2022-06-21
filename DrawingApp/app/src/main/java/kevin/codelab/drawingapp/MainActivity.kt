109356c73e6ad789fc9d2353b9e743dc105af4f4
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-21 04:23:54 +0000

    enabled viewBinding

--- DrawingApp/app/src/main/java/kevin/codelab/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/kevin/codelab/drawingapp/MainActivity.kt
@@ -2,10 +2,16 @@ package kevin.codelab.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kevin.codelab.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // enabled viewBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
