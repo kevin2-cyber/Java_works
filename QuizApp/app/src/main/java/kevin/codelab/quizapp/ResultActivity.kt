5c9cf3661ddbc111faa45e5adba3340bbec94298
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-21 03:36:25 +0000

    sending data via intent - putExtra

--- QuizApp/app/src/main/java/kevin/codelab/quizapp/ResultActivity.kt
+++ QuizApp/app/src/main/java/kevin/codelab/quizapp/ResultActivity.kt
@@ -1,5 +1,6 @@
 package kevin.codelab.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kevin.codelab.quizapp.databinding.ActivityResultBinding
@@ -12,5 +13,16 @@ class ResultActivity : AppCompatActivity() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.tvName.text = intent.getStringExtra(Constants.USER_NAME)
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        binding.tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
+
+        binding.btnFinish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
