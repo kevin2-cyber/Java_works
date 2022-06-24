13b4a97a7c41aa1a49e760feb23bc762c364792f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-24 18:05:34 +0000

    new

--- DrawingApp/app/src/main/java/kevin/codelab/drawingapp/DrawingView.kt
+++ DrawingApp/app/src/main/java/kevin/codelab/drawingapp/DrawingView.kt
@@ -104,6 +104,10 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         mDrawPaint!!.strokeWidth = mBrushSize
     }
 
+    fun setColor(newColor: String) {
+        color = Color.parseColor(newColor)
+    }
+
     internal inner class CustomPath(var color: Int,
                                     var brushThickness: Float) : Path() {
 
