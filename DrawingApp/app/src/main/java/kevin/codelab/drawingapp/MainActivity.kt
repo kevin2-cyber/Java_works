5053326cc3c26a65d8518126af29aa89cbec9684
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-24 15:26:20 +0000

    selected brush size from custom dialog created

--- DrawingApp/app/src/main/java/kevin/codelab/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/kevin/codelab/drawingapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package kevin.codelab.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import kevin.codelab.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
+        // binding.drawingView.setSizeForBrush(20.toFloat())
 
-        binding.drawingView.setSizeForBrush(20.toFloat())
+        binding.ibBrush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        smallBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
