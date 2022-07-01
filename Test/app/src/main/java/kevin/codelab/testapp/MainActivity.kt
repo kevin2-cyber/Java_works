db82f82a1f57f826b6f704a21f4db457d27a8615
kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-01 22:10:41 +0000

    checked if the Text field is not empty

--- Test/app/src/main/java/kevin/codelab/testapp/MainActivity.kt
+++ Test/app/src/main/java/kevin/codelab/testapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 const val EXTRA_MESSAGE = "kevin.codelab.testapp.MESSAGE"
 
@@ -19,9 +20,16 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
         // val button = findViewById<Button>(R.id.button)
         val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+
+        if (editText.text.isEmpty()) {
+            Toast.makeText(this,"Please enter a message", Toast.LENGTH_LONG).show()
+        } else {
+            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
+
         }
-        startActivity(intent)
+
     }
 }
