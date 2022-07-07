a4dc8975a2bbf998d3619b149b30153ef5050248
kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-07 01:26:04 +0000

    new

--- KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/ListViewActivity.java
+++ KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/ListViewActivity.java
@@ -1,12 +1,13 @@
 package kevin.codelab.kidsdrawingapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListViewActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
     }
--- KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/MainActivity.java
+++ KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton mImageButtonCurrentPaint;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
