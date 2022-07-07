61f17e7df2374fda9755ff6b9b0af13b9bf4f7ac
kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-07 03:11:35 +0000

    new

--- KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/MainActivity.java
+++ KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/MainActivity.java
@@ -1,16 +1,20 @@
 package kevin.codelab.kidsdrawingapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 import kevin.codelab.kidsdrawingapp.databinding.ActivityMainBinding;
 
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding = null;
     private @Nullable
     ImageButton mImageButtonCurrentPaint;
+
+    ActivityResultLauncher<String> requestPermission = registerForActivityResult(new ActivityResultContracts.RequestPermission()) {}
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void showBrushSizeChooserDialog() {
+        Dialog brushDialog = new Dialog(this);
+        brushDialog.setContentView(R.layout.dialog_brush_size);
+        brushDialog.setTitle("Brush Size: ");
+        ImageButton smallBtn = brushDialog.findViewById(R.id.ib_small_brush);
+        ImageButton mediumBtn = brushDialog.findViewById(R.id.ib_medium_brush);
+        ImageButton largeBtn = brushDialog.findViewById(R.id.ib_large_brush);
+
+        smallBtn.setOnClickListener(
+                view -> {
+                    binding.drawingView.setSizeForBrush(10.0F);
+                    brushDialog.dismiss();
+                }
+        );
+
+        mediumBtn.setOnClickListener(
+                view -> {
+                    binding.drawingView.setSizeForBrush(20.0F);
+                    brushDialog.dismiss();
+                }
+        );
+
+        largeBtn.setOnClickListener(
+                view -> {
+                    binding.drawingView.setSizeForBrush(30.0F);
+                    brushDialog.dismiss();
+                }
+        );
+
+        brushDialog.show();
+    }
+
     private void paintClicked(View view) {
         Toast.makeText(this,"Paint clicked", Toast.LENGTH_LONG).show();
         if (view != mImageButtonCurrentPaint){
@@ -75,5 +113,29 @@ public class MainActivity extends AppCompatActivity {
 
             mImageButtonCurrentPaint = (ImageButton) view;
         }
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            showRationaleDialog("Kids Drawing App",
+                    "Kids Drawing App needs Access to your External Storage");
+        } else {
+            requestPermission.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+    private void showRationaleDialog(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Cancel", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        builder.create().show();
+
     }
 }
