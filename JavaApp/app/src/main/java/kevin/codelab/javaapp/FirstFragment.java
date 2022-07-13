984a13bc68ab00aeaeace646a2ed953ec0cc2672
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-13 02:22:43 +0000

    bugs to fix

--- JavaApp/app/src/main/java/kevin/codelab/javaapp/FirstFragment.java
+++ JavaApp/app/src/main/java/kevin/codelab/javaapp/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,14 +16,17 @@ import kevin.codelab.javaapp.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    TextView showCountTextView;
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        // Get the count text view
+        showCountTextView = binding.textviewFirst;
         return binding.getRoot();
 
     }
@@ -29,13 +34,27 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.randomButton.setOnClickListener(view1 ->
+                // FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+                .navigate(R.id.action_FirstFragment_to_SecondFragment));
+
+        binding.toastButton.setOnClickListener(view2 ->
+                Toast.makeText(getActivity(), "Hello Toast!", Toast.LENGTH_SHORT).show()
+        );
+
+        binding.countButton.setOnClickListener(this::countMe);
+
+    }
+
+    private void countMe(View view) {
+        // Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        // Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
     }
 
     @Override
--- JavaApp/app/src/main/java/kevin/codelab/javaapp/SecondFragment.java
+++ JavaApp/app/src/main/java/kevin/codelab/javaapp/SecondFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.Random;
 
 import kevin.codelab.javaapp.databinding.FragmentSecondBinding;
 
@@ -17,7 +20,7 @@ public class SecondFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
@@ -29,13 +32,23 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.buttonSecond.setOnClickListener(view1 ->
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+                .navigate(R.id.action_SecondFragment_to_FirstFragment));
+
+        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.random_heading, count);
+        TextView headerView = view.getRootView().findViewById(R.id.textview_header);
+        headerView.setText(countText);
+
+        Random random = new Random();
+        Integer randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+
+        TextView randomView = view.getRootView().findViewById(R.id.textview_random);
+        randomView.setText(randomNumber.toString());
     }
 
     @Override
