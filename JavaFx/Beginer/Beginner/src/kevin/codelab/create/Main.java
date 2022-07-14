f5da780f309b7a71598c2bbc46bc196929296b8e
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-14 03:09:49 +0000

    new

---
+++ JavaFx/Beginer/Beginner/src/kevin/codelab/create/Main.java
@@ -0,0 +1,9 @@
+package kevin.codelab.create;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int age = 12;
+        System.out.println(age);
+    }
+}
