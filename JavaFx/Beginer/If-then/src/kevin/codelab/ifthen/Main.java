07f14e74e21f003949fbb48ad1a6d630670a52fe
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-15 12:34:41 +0000

    if then statements

--- JavaFx/Beginer/If-then/src/kevin/codelab/ifthen/Main.java
+++ JavaFx/Beginer/If-then/src/kevin/codelab/ifthen/Main.java
@@ -3,8 +3,7 @@ package kevin.codelab.ifthen;
 public class Main {
     public static void main(String[] args) {
         boolean isAlien = false;
-        if (isAlien == false){
+        if (isAlien == true)
             System.out.println("it is not an alien");
-        }
     }
 }
