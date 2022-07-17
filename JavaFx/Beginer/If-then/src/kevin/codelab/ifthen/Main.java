6a296a0ab235a06a010044a6c4174e503c718c1c
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-17 21:38:43 +0000

    del

--- JavaFx/Beginer/If-then/src/kevin/codelab/ifthen/Main.java
+++
@@ -1,9 +0,0 @@
-package kevin.codelab.ifthen;
-
-public class Main {
-    public static void main(String[] args) {
-        boolean isAlien = false;
-        if (isAlien == true)
-            System.out.println("it is not an alien");
-    }
-}
