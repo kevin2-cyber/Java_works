f385f3f8e4a4bf4541832c89f4d0a5d689110d8e
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-17 21:03:53 +0000

    bug on splash screen

--- SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
@@ -2,15 +2,22 @@ package kevin.codelab.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kevin.codelab.splashscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition(
+                viewModel.isLoading.value
+            )
+        }
         setContentView(binding.root)
     }
 }
