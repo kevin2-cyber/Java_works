6feeb97184740a8869598ac7a35914d77300f577
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-21 12:41:06 +0000

    tutorials

--- KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/ListViewActivity.java
+++ KidsDrawingApp/app/src/main/java/kevin/codelab/kidsdrawingapp/ListViewActivity.java
@@ -4,11 +4,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kevin.codelab.kidsdrawingapp.databinding.ActivityListviewBinding;
+
 public class ListViewActivity extends AppCompatActivity {
+
+    @Nullable
+    private ActivityListviewBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        binding = ActivityListviewBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listview);
+        setContentView(binding.getRoot());
     }
 }
