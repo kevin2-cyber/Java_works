58a81d4e886dd9921cc412e53113da831f3dafb4
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-24 19:42:08 +0000

    new

--- NotesApp/app/src/main/java/kevin/codelab/notesapp/HomeActivity.java
+++ NotesApp/app/src/main/java/kevin/codelab/notesapp/HomeActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import kevin.codelab.notesapp.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(binding.getRoot());
     }
 }
--- NotesApp/app/src/main/java/kevin/codelab/notesapp/SignUpActivity.java
+++ NotesApp/app/src/main/java/kevin/codelab/notesapp/SignUpActivity.java
@@ -2,13 +2,30 @@ package kevin.codelab.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import kevin.codelab.notesapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private ActivitySignUpBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(binding.getRoot());
+
+        binding.acbSignUp.setOnClickListener(
+                view ->
+                        firebaseSignUp()
+        );
+    }
+
+    private void firebaseSignUp() {
+        startActivity(
+                new Intent(this, HomeActivity.class)
+        );
     }
 }
