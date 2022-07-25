1b3fda53e39884278d6f108a338fe4d77c1a9796
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-25 22:52:38 +0000

    new

--- NotesApp/app/src/main/java/kevin/codelab/notesapp/MainActivity.java
+++ NotesApp/app/src/main/java/kevin/codelab/notesapp/MainActivity.java
@@ -1,9 +1,11 @@
 package kevin.codelab.notesapp;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        getSplashScreen().setSplashScreenTheme(R.style.Theme_AppCompat_SplashDay);
         setContentView(binding.getRoot());
 
         binding.signUpBtn.setOnClickListener(
