e28defd1c5c4e0afd056f964990a9353bb9a7c9a
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-25 02:03:47 +0000

    new

--- NotesApp/app/src/main/java/kevin/codelab/notesapp/HomeActivity.java
+++ NotesApp/app/src/main/java/kevin/codelab/notesapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package kevin.codelab.notesapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,10 +9,11 @@ import kevin.codelab.notesapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ActivityHomeBinding binding;
+    private @Nullable
+    ActivityHomeBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
--- NotesApp/app/src/main/java/kevin/codelab/notesapp/MainActivity.java
+++ NotesApp/app/src/main/java/kevin/codelab/notesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package kevin.codelab.notesapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,10 +13,10 @@ import kevin.codelab.notesapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private @Nullable ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
