64d56bd1902873b268dedb2722a1043d5a9ecab9
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-25 04:25:36 +0000

    initia commit

---
+++ Custom_ListView/app/src/main/java/kevin/codelab/custom_listview/MainActivity.java
@@ -0,0 +1,14 @@
+package kevin.codelab.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
