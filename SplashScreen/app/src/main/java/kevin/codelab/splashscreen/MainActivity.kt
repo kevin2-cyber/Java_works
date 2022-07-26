6bc2225136427cd1f7b9b0224410613159f12f1e
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-26 17:36:05 +0000

    added an animation

--- SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
@@ -1,16 +1,41 @@
 package kevin.codelab.splashscreen
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import kevin.codelab.splashscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 200L
+
+            slideUp.doOnEnd {
+                splashScreenView.remove()
+            }
+
+            slideUp.start()
+        }
+
         setContentView(binding.root)
     }
 }
