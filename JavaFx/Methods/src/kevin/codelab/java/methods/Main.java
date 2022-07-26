8c9fd1713762bf99bff74fb8c35bb293563f707c
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-26 12:24:31 +0000

    minor change

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -12,11 +12,11 @@ public class Main {
         calculateScore(true, 10000, 8, 200);
 
 
-        int[] num2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        for (int i = 0; i < num2.length; i++) {
-            System.out.println("i = " + i);
-        }
+//        int[] num2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//
+//        for (int i = 0; i < num2.length; i++) {
+//            System.out.println("i = " + i);
+//        }
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
