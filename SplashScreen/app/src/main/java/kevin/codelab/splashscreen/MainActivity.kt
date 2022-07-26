6d1ee848aa522861674941143c75e79cf6f8a4df
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-26 17:17:25 +0000

    new

--- SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/kevin/codelab/splashscreen/MainActivity.kt
@@ -1,21 +1,16 @@
 package kevin.codelab.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kevin.codelab.splashscreen.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
     }
-
-
 }
