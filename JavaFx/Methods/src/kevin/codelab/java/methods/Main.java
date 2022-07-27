bf405ac0df6e2bab846a852c5988db9cbeb70147
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-27 20:37:47 +0000

    new

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -18,12 +18,15 @@ public class Main {
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
     }
 }
