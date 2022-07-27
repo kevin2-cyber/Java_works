63741d5e8cdc1c8b44b4a9f9c0cda330928e9df0
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-27 20:51:05 +0000

    returning something

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -2,11 +2,21 @@ package kevin.codelab.java.methods;
 
 public class Main {
     public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
 
-        calculateScore(true, 800, 5, 100);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
-        calculateScore(true, 10000, 8, 200);
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
 
 //        int[] num2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -23,7 +33,6 @@ public class Main {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
