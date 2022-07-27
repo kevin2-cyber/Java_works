3ee68ae8c26a15119a2ed619f54ee97f9af90661
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-27 21:36:15 +0000

    created calculatedHighScorePosition method

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -38,8 +38,13 @@ public class Main {
 
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore > 1000) {
-
+            return 1;
+        } else if (playerScore > 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore > 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
         }
-        return 0;
     }
 }
