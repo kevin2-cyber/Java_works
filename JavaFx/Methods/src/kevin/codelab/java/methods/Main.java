cbe3edce7fe24d5e6acf28c09df9b11b1970b4b6
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-02 16:44:23 +0000

    using a single return statement instead multiple

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -55,14 +55,25 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+        int position = 4;
+
         if (playerScore >= 1000) {
-            return 1;
+            position = 1;
         } else if (playerScore >= 500) {
-            return 2;
+            position = 2;
         } else if (playerScore >= 100) {
-            return 3;
-        } else {
-            return 4;
+            position = 3;
         }
+
+        return position;
     }
 }
