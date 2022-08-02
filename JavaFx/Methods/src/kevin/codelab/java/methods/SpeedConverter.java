24508b9128929f464c5c041b7ec9e6f9b6b48707
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-02 19:50:13 +0000

    new

--- JavaFx/Methods/src/kevin/codelab/java/methods/SpeedConverter.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/SpeedConverter.java
@@ -2,4 +2,13 @@ package kevin.codelab.java.methods;
 
 public class SpeedConverter {
 
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("invalid value");
+            return -1;
+        }
+
+        return 1;
+    }
 }
