6c957dc517428ebe42f9b6c0e9b5e6cac91728bd
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-02 16:35:09 +0000

    fixed logical error bug

--- JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/Main.java
@@ -57,9 +57,9 @@ public class Main {
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
         } else {
             return 4;
