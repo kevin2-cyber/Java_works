568f546056b02877488f5a7933b46286c07c9f44
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-02 23:22:03 +0000

    created first method

--- JavaFx/Methods/src/kevin/codelab/java/methods/SpeedConverter.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/SpeedConverter.java
@@ -7,11 +7,13 @@ public class SpeedConverter {
         if (kilometersPerHour < 0) {
             System.out.println("invalid value");
             return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
         }
-
-        return 1;
     }
 
+
+    // 1 Mile per Hour is 1.609 kilometers per hour
     public static void printConversion(double kilometersPerHour) {
 
     }
