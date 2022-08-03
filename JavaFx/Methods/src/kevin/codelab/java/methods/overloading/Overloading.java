857fbe32890806ec9f9285b613ee0d7f4e3c5df0
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-03 19:14:44 +0000

    new

--- JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
@@ -3,6 +3,13 @@ package kevin.codelab.java.methods.overloading;
 public class Overloading {
 
     public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
 
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
     }
 }
