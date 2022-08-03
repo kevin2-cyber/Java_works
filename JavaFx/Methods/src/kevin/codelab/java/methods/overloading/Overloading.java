c6501fd74ec364acadec9132171d2eadcdb92fec
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-03 19:17:55 +0000

    never knew about overloading

--- JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
@@ -12,4 +12,9 @@ public class Overloading {
         System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
     }
+
+    public static int calculateScore( int score) {
+        System.out.println("Unnamed Player scored " + score + " points");
+        return score * 1000;
+    }
 }
