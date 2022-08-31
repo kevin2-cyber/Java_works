72d51fd91c62b0e844279cda27936b9933391db9
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-31 11:34:12 +0000

    almost done with Main

--- JavaFx/App/src/com/digerati/upmap/Main.java
+++ JavaFx/App/src/com/digerati/upmap/Main.java
@@ -1,3 +1,130 @@
 package com.digerati.upmap;
 
-public class Main {}
+import com.digerati.upmap.graph.Edge;
+import com.digerati.upmap.graph.Graph;
+import com.digerati.upmap.graph.Node;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        // Nodes in the graph.
+        Node gym = new Node("Gym");
+        Node diaspora = new Node("Diaspora");
+        Node ish = new Node("ISH");
+        Node nightMarket = new Node("Night Market");
+        Node sarbahHall = new Node("Sarbah Hall");
+        Node commonWealth = new Node("Common Wealth");
+        Node greatHall = new Node("Great Hall");
+        Node legonHall = new Node("Legon Hall");
+        Node akuafoHall = new Node("Akuafo Hall");
+        Node voltaHall = new Node("Volta Hall");
+        Node balmeLibrary = new Node("Balme Library");
+        Node cbas = new Node("CBAS");
+        Node mainGate = new Node("Main Gate");
+        Node jqb = new Node("JQ Building");
+        Node lawSchool = new Node("Law School");
+        Node busSchool = new Node("Business School");
+        Node gcb = new Node("GCB");
+        Node csdepartment = new Node("CS Department");
+        Node polictialScienceDepartment = new Node("Political Science Department");
+        Node nb = new Node("NB");
+        Node nnb = new Node("NNB");
+
+        graph.addEdge(new Edge(gym, diaspora, 500, 10, "George Benne Round About"));
+        graph.addEdge(new Edge(gym, ish, 415, 8,"George Benne Round About"));
+        graph.addEdge(new Edge(gym, nightMarket, 634, 9,"George Benne Round About"));
+
+        graph.addEdge(new Edge(diaspora, ish, 400, 8, "grassland"));
+        graph.addEdge(new Edge(ish, nightMarket, 214, 3, "banking square, banks, atm"));
+
+        graph.addEdge(new Edge(nightMarket, commonWealth, 1025, 20, "sarbah field"));
+        graph.addEdge(new Edge(nightMarket, legonHall, 914, 17, "sarbah field"));
+        graph.addEdge(new Edge(nightMarket, sarbahHall, 350, 5));
+
+        graph.addEdge(new Edge(commonWealth, greatHall, 515, 9, "greater hall tower"));
+        graph.addEdge(new Edge(commonWealth, voltaHall, 440, 5, "atm"));
+        graph.addEdge(new Edge(commonWealth, legonHall, 460, 5, "atm"));
+
+        graph.addEdge(new Edge(sarbahHall, legonHall, 630, 12, "athletic oval, sarbah field"));
+        graph.addEdge(new Edge(sarbahHall, akuafoHall, 460, 8, "athletic oval"));
+
+        graph.addEdge(new Edge(legonHall, akuafoHall, 583, 7, "absa bank"));
+        graph.addEdge(new Edge(legonHall, balmeLibrary, 530, 6, "economic department"));
+        graph.addEdge(new Edge(legonHall, voltaHall, 260, 3, "atm"));
+
+        graph.addEdge(new Edge(akuafoHall, cbas, 385, 5, "agric department"));
+        graph.addEdge(new Edge(akuafoHall, csdepartment, 780, 13, "chemistry department"));
+        graph.addEdge(new Edge(akuafoHall, balmeLibrary, 580, 7, "absa bank"));
+
+        graph.addEdge(new Edge(cbas, mainGate, 624, 6));
+        graph.addEdge(new Edge(cbas, jqb, 610, 9));
+        graph.addEdge(new Edge(jqb, lawSchool, 466, 5));
+
+        graph.addEdge(new Edge(lawSchool, csdepartment, 384, 4, "mathematics department"));
+
+        graph.addEdge(new Edge(balmeLibrary, lawSchool, 960, 18));
+        graph.addEdge(new Edge(balmeLibrary, busSchool, 203, 4));
+
+        graph.addEdge(new Edge(voltaHall, busSchool, 390, 3));
+        graph.addEdge(new Edge(voltaHall, balmeLibrary, 415, 5));
+
+        graph.addEdge(new Edge(voltaHall, balmeLibrary, 415, 5));
+
+        graph.addEdge(new Edge(busSchool, gcb, 433, 5, "balme library fountains"));
+        graph.addEdge(new Edge(busSchool, nb, 424, 5, "balme library fountains"));
+        graph.addEdge(new Edge(busSchool, csdepartment, 389, 4));
+
+        graph.addEdge(new Edge(polictialScienceDepartment, csdepartment, 386, 4));
+        graph.addEdge(new Edge(polictialScienceDepartment, nb, 204, 3));
+        graph.addEdge(new Edge(nb, nnb, 330, 4));
+        graph.addEdge(new Edge(nnb, gcb, 160, 3));
+
+        // MAP GUI
+        UgMap ugMap = new UgMap();
+        JFrame mapFrame = new JFrame();
+        mapFrame.add(ugMap);
+        mapFrame.setTitle("UG MAP");
+        mapFrame.setSize(850, 774);
+        mapFrame.setVisible(true);
+
+        // GUI
+        final int WINDOW_WIDTH = 800;
+        final int WINDOW_HEIGHT = 600;
+
+        String[] places = new String[graph.getNodeSize()];
+        int index = 0;
+        for (Node node :
+                graph.getNodes()) {
+            places[index] = node.getName();
+            index++;
+        }
+
+        JFrame frame = new JFrame();
+        frame.setTitle("UG MAP: Controls UI");
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Source
+        JComboBox<String> sourceSelectionBox = new JComboBox<>(places);
+        JLabel sourceLabel = new JLabel("CurrentLocation");
+        sourceLabel.setForeground(Color.MAGENTA);
+        sourceLabel.setBounds(10, 10, WINDOW_WIDTH/2-100, 20);
+        sourceSelectionBox.setBounds(5, 20, WINDOW_WIDTH/2-100, 40);
+        frame.add(sourceSelectionBox);
+        frame.add(sourceLabel);
+
+        // Destination
+        JComboBox<String> destinationSelectionBox = new JComboBox<>(places);
+        destinationSelectionBox.setSelectedIndex(2);
+        JLabel destinationLabel = new JLabel("I'm going to: ");
+        destinationLabel.setForeground(Color.MAGENTA);
+        destinationLabel.setBounds(WINDOW_WIDTH/2 + 50, 10, WINDOW_WIDTH/2 - 100, 20);
+        destinationSelectionBox.setBounds(WINDOW_WIDTH/2 + 50, 20, WINDOW_WIDTH/2-100, 40);
+        frame.add(destinationSelectionBox);
+        frame.add(destinationLabel);
+    }
+}
