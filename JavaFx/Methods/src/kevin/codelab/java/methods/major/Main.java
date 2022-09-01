d2c9047a15af84096d5c5a5cafa6f819bc4dfce4
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-01 23:18:20 +0000

    new

--- JavaFx/Methods/src/kevin/codelab/java/methods/major/Main.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/major/Main.java
@@ -63,15 +63,6 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-//        if (playerScore >= 1000) {
-//            return 1;
-//        } else if (playerScore >= 500) {
-//            return 2;
-//        } else if (playerScore >= 100) {
-//            return 3;
-//        } else {
-//            return 4;
-//        }
 
         int position = 4;
 
