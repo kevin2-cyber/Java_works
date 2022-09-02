a759979799102356a867d505d3285f2203ee0dbb
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-02 02:46:56 +0000

    new

--- JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
@@ -7,6 +7,7 @@ public class Overloading {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, 0);
 
     }
 
@@ -25,5 +26,39 @@ public class Overloading {
         return 0;
     }
 
-    public static void calcFeetAndInchesToCentimeters(int feet, int inches){}
+    // Create a method called calcFeetAndInchesToCentimeters
+    // It needs to have two parameters.
+    // feet is the 1st parameter, inches is the 2nd parameter
+
+    // You should validate that the 1st parameter feet is >=0
+    // You should validate that the 2nd parameter inches is >=0 and <=12
+    // return -1 from the method if either of the above is not true
+
+    // If the parameters are valid, then calculate how many centimetres
+    // comprise the feet and inches passed to this method and return that value.
+
+    // Create a 2nd method of the same name but with only one parameter
+    // inches is the parameter
+    // validate that it's >=0
+    // return -1 if is not true
+    // But if it's valid, then calculate how many feet are in the inches
+    // and then here is the tricky part
+    // call the other overloaded method passing the correct feet and inches
+    // calculated so that it can calculate correctly
+    // hints: Use double for your number dataTypes is probably a good idea
+    // 1 inch = 2.54cm and one foot = 12inches
+    // use the link I give you to confirm your code is calculating correctly.
+    // Calling another overloaded method just requires you to use the
+    // right number of parameters.
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || (inches < 0) || (inches > 12) ) {
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
 }
