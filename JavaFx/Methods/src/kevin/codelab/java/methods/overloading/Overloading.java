7307effba96ad34648d7ffa4fce563b9f046798c
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-03 00:14:17 +0000

    initial commit for the day

--- JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
@@ -77,4 +77,17 @@ public class Overloading {
         System.out.println(inches + " inches  is equal to " + feet + " feet and " + remainingInches + "inches");
         return calcFeetAndInchesToCentimeters(feet, remainingInches);
     }
+
+    // Create a method called getDurationString with two parameters, first parameter
+    // minutes and 2nd parameter seconds
+    // You should validate that the first parameter minutes is >=0
+    // You should validate that the 2nd parameter seconds is >=0 and <=59
+    // The method should return "Invalid value" in the method if either of the above are not true
+    // If the parameters are valid then calculate how many hours, minutes and seconds
+    // equal the minutes and seconds passed to this method and return that value as
+    // string format "XXh YYm ZZs" where XX represents a number of hours, YY the minutes
+    // and ZZ the seconds.
+
+    // Create a 2nd method of the same name but with only one parameter seconds.
+    // Validate that it is >=0, and return "Invalid value" if it is not true.
 }
