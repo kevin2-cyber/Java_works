60af8a11f1589a4a7edd59eb136683a23a921aed
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-05 03:25:43 +0000

    added bonus

--- JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
+++ JavaFx/Methods/src/kevin/codelab/java/methods/overloading/Overloading.java
@@ -109,7 +109,21 @@ public class Overloading {
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if (hours < 10)
+            hoursString = "0" + hoursString;
+
+
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10)
+            minutesString = "0" + minutesString;
+
+        String secondsString = seconds + "s";
+        if (seconds < 10)
+            secondsString = "0" + secondsString;
+
+        return hoursString+ " " + minutesString + " " + secondsString + " ";
     }
 
     private static String getDurationString(long seconds) {
