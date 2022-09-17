e9219355909e847d4f9138381d2ef492f853e931
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-17 12:38:34 +0000

    Euclid Algorithm working

--- Jakarta/src/algol/algol/Euclid.java
+++ Jakarta/src/algol/algol/Euclid.java
@@ -2,6 +2,19 @@ package algol.algol;
 
 public class Euclid {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int eu = euclid(60, 24);
+        System.out.println(eu);
+        int test2 = euclid(90, 40);
+        System.out.println(test2);
+    }
+
+    private static int euclid(int m, int n) {
+        int r = 0;
+        while (n  != 0) {
+            r = m % n;
+            m = n;
+            n = r;
+        }
+        return m;
     }
 }
