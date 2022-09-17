a274b23ccce915b68651652fa30e2ab3c6103f51
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-17 12:43:20 +0000

    new

--- Jakarta/src/algol/algol/Test.java
+++ Jakarta/src/algol/algol/Test.java
@@ -1,20 +1,15 @@
 package algol.algol;
 
+import static algol.algol.Euclid.euclid;
+
 public class Test {
     public static void main(String[] args) {
+
         int eu = euclid(60, 24);
         System.out.println(eu);
         int test2 = euclid(90, 40);
         System.out.println(test2);
     }
 
-    private static int euclid(int m, int n) {
-        int r = 0;
-        while (n  != 0) {
-            r = m % n;
-            m = n;
-            n = r;
-        }
-        return m;
-    }
+
 }
