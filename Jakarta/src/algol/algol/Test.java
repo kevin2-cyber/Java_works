2e4be16af37927e31e74238d418815c7fc6f975a
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-19 00:51:28 +0000

    stackoverflow errors

--- Jakarta/src/algol/algol/Test.java
+++ Jakarta/src/algol/algol/Test.java
@@ -1,6 +1,7 @@
 package algol.algol;
 
 import static algol.algol.Euclid.euclid;
+import static algol.algol.Sorting.mergeSort;
 
 public class Test {
     public static void main(String[] args) {
@@ -9,6 +10,8 @@ public class Test {
         System.out.println(eu);
         int test2 = euclid(90, 40);
         System.out.println(test2);
+        mergeSort(new int[]{2, 4, 6, 7, 3, 1, 3, 2, 4}, 4, 8);
+
     }
 
 
