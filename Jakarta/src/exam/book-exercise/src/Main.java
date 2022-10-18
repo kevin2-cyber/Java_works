89021e3da436d9fa12aaf47925a2cf311609dd37
kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-18 17:53:11 +0000

    created an instance of the class Book

--- Jakarta/src/exam/book-exercise/src/Main.java
+++ Jakarta/src/exam/book-exercise/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book = new Book("Kofi", "Name", 10);
+        System.out.println(book.getAuthor());
     }
 }
