4da3dbb7ee4a0dd30f768c0b4c5fbdb11e3e4b12
kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-21 21:31:46 +0000

    number display

---
+++ Jakarta/src/exam/clock-display/src/com/exam/main/NumberDisplay.java
@@ -0,0 +1,38 @@
+package com.exam.main;
+
+public class NumberDisplay {
+
+    private int limit;
+    private int value;
+
+    public NumberDisplay(int rollOverLimit){
+        limit = rollOverLimit;
+        value = 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDisplayValue(){
+        if (value < 10) {
+            return "0" + value;
+        } else {
+            return "" + value;
+        }
+    }
+
+    public void setValue(int replacementValue) {
+        if ((replacementValue >= 0) || (replacementValue < limit)) {
+            value = replacementValue;
+        }
+    }
+
+    public void increment() {
+        if (value >= limit) {
+            value = 0;
+        } else {
+            value++;
+        }
+    }
+}
