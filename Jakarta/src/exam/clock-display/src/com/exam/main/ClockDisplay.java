3f85ebfae4ddbd4a3b8b7c397eae173620b9241a
kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-23 08:05:29 +0000

    completed clock-display

--- Jakarta/src/exam/clock-display/src/com/exam/main/ClockDisplay.java
+++ Jakarta/src/exam/clock-display/src/com/exam/main/ClockDisplay.java
@@ -13,4 +13,38 @@ public class ClockDisplay {
         seconds = new NumberDisplay(59);
         updateDisplay();
     }
+
+    public ClockDisplay(int hour, int minute, int second) {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(59);
+        seconds = new NumberDisplay(59);
+        setTime(hour, minute, second);
+    }
+
+    public void timeTick() {
+        seconds.increment();
+        if (seconds.getValue() == 0) {
+            minutes.increment();
+        }
+
+        updateDisplay();
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        hours.setValue(hour);
+        minutes.setValue(minute);
+        seconds.setValue(second);
+
+        updateDisplay();
+    }
+
+    public String getTime() {
+        return displayString;
+    }
+
+    public void updateDisplay() {
+        displayString = hours.getDisplayValue() + ":" +
+                        minutes.getDisplayValue() + ":" +
+                        seconds.getDisplayValue();
+    }
 }
