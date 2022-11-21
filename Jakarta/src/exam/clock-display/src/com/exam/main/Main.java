1fac7fa9ae68451afcd8131a32a4dc538aee9d29
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-21 11:33:40 +0000

    tried to use the scanner class to input the time

--- Jakarta/src/exam/clock-display/src/com/exam/main/Main.java
+++ Jakarta/src/exam/clock-display/src/com/exam/main/Main.java
@@ -1,9 +1,12 @@
 package com.exam.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ClockDisplay display = new ClockDisplay();
-        display.setTime(1, 20, 20);
+        display.setTime(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
         System.out.println(display.getTime());
     }
 }
